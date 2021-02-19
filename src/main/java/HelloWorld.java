20d2a73798e4df6f993bc36aa03996d01dc366a2
houfaxing <873782943@qq.com>
2021-02-19 12:29:45 +0800

    aaaa

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @program: day1
+ * @description:
+ * @author: Mr.Hou
+ * @create: 2021-02-19 12:15
+ **/
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
+
