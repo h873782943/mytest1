e4c786a3877bfd9ef89323d0f2403e785b6849b8
houfaxing <873782943@qq.com>
2021-02-19 12:53:32 +0800

    添加一个工程

---
+++ day1/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @program: day1
+ * @description:
+ * @author: Mr.Hou
+ * @create: 2021-02-19 12:51
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+    }
+}
+
